73853b8480c359bc2cf3c06071d848435f9bfe75
Jonathan Jalfon <jonyjalfon94@gmail.com>
2022-03-13 11:59:58 +0200

    feat: Add new stuff

--- app/main.py
+++ app/main.py
@@ -20,6 +20,11 @@ def read_root():
     return {"Hello": "World!"}
 
 
+@app.get("/new")
+def read_root():
+    return {"New": "World!"}
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
