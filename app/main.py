805beeb3118d607e0ed2d8d0f20fa90c842f4f85
Jonathan Jalfon <jonyjalfon94@gmail.com>
2022-03-13 12:01:12 +0200

    feat: Even more features added

--- app/main.py
+++ app/main.py
@@ -25,6 +25,9 @@ def read_root():
     return {"New": "World!"}
 
 
+@app.get("/new-2")
+def read_root():
+    return {"New2": "World!"}
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
